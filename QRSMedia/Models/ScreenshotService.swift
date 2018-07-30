667afcf8b08b89e5896b5448f6dc875c9660d247
Emily Lam <emilylam1668@gmail.com>
2018-07-29 23:45:57 -0700

    ERICK firebase explanation

---
+++ QRSMedia/Models/ScreenshotService.swift
@@ -0,0 +1,16 @@
+//
+//  ScreenshotService.swift
+//  QRSMedia
+//
+//  Created by Emily Lam on 7/29/18.
+//  Copyright © 2018 Make School. All rights reserved.
+//
+
+import Foundation
+
+struct ScreenshotService {
+    //upload image to database and save the url into UserDefaults
+    
+    //get screenshot from the database for the current device, if url is in UserDefaults
+    
+}
---
+++ QRSMedia/Models/StorageService.swift
@@ -0,0 +1,47 @@
+//
+//  StorageService.swift
+//  QRSMedia
+//
+//  Created by Emily Lam on 7/29/18.
+//  Copyright © 2018 Make School. All rights reserved.
+//
+
+import Foundation
+import UIKit
+//import FirebaseDatabase
+//
+//struct StorageService {
+//    public static func uploadSSImage(_ image: UIImage, at reference: StorageReference, completion: @escaping (URL?) -> ()) {
+//        let refToFirebaseDatabase = Database.database().reference()
+//
+//        let refToUsers = refToFirebaseDatabase.child("users")
+//
+//        refToUsers.observeSingleEvent(of: .value) { snapshot in
+//            guard let dicationOfUsers = snapshot.value as? [String: Any] else {
+//                return assertionFailure("this broke ☠️")
+//            }
+//            let listOfUser
+//
+//            let myVariable: [String: Any] = [
+//                "myKey": "",
+//                "otherKey": 98
+//            ]
+//
+//
+//
+//
+//            let dictOfUsers = [
+//                "username": "ErickES7"
+//            ]
+//
+//            let username = dictOfUsers["username"]
+//            
+//            let dictOfUser = [
+//                "users": [
+//                    "asdfghjktrew": ["age": 17, "username": "ErickES7"],
+//                    "poiuytgkjhgkk": ["age": 17, "username": "ErickES7"]
+//                ]
+//            ]
+//        }
+//    }
+//}
