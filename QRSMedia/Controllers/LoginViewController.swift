8e5fe646d1cbe4ad8b93db02ab1eb17b230a6c13
Emily Lam <emilylam1668@gmail.com>
2018-08-06 09:11:50 -0700

    changed login view background to gradient

--- QRSMedia/Controllers/LoginViewController.swift
+++ QRSMedia/Controllers/LoginViewController.swift
@@ -11,12 +11,17 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    var gradientLayer: CAGradientLayer!
+    
+    @IBOutlet weak var gradientView: UIView!
     @IBOutlet weak var doneButton: UIButton!
     @IBOutlet weak var insturctionsButton: UIButton!
     @IBOutlet weak var QRSMLogoImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        createGradientLayer()
+
         self.QRSMLogoImage.image = #imageLiteral(resourceName: "qrsm-transparent")
     }
     
@@ -25,6 +30,17 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func instructionsButtonTapped(_ sender: UIButton) {
+    }
+    
+    func createGradientLayer() {
+        gradientLayer = CAGradientLayer()
+        gradientLayer.frame = self.view.bounds
+        
+        let pink = UIColor(red: 188/255.0, green: 100/255.0, blue: 146/255.0, alpha: 1)
+        let blue = UIColor(red: 94/255.0, green: 122/255.0, blue: 168/255.0, alpha: 1)
+        
+        gradientLayer.colors = [blue.cgColor, pink.cgColor]
+        gradientView.layer.addSublayer(gradientLayer)
     }
     
 }
